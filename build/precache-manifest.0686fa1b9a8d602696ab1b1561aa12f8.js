e3f7e0f7d56fb63e6513570f7bba8556b54d7ff0
Thomas Neppelenbroek <thomas@newzoo.com>
2020-04-16 19:19:49 +0200

    build

---
+++ build/precache-manifest.0686fa1b9a8d602696ab1b1561aa12f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8fe648b18d987e4d1180ad3cb8dabb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f786ff9fb9489c6cadec",
+    "url": "/static/css/main.0d57ca01.chunk.css"
+  },
+  {
+    "revision": "a0aded6cc4bfbe532ae4",
+    "url": "/static/js/2.d84d2689.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.d84d2689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f786ff9fb9489c6cadec",
+    "url": "/static/js/main.be0f7035.chunk.js"
+  },
+  {
+    "revision": "6aa9d9146da17af0a53e",
+    "url": "/static/js/runtime-main.ea732b8c.js"
+  },
+  {
+    "revision": "00b1ad9ef2021ec0e3c0352467c29182",
+    "url": "/static/media/reset.00b1ad9e.svg"
+  },
+  {
+    "revision": "ddb4778aa6cd2dca2fd08494b5fb14d6",
+    "url": "/static/media/snoeien.ddb4778a.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3778bbedeb07ea04511f80e48741890b.js"
+  "/precache-manifest.0686fa1b9a8d602696ab1b1561aa12f8.js"
 );
 
 self.addEventListener('message', (event) => {
